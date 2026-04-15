3ed82bad1580289674ff32062ba4f8036cd0459a
rithehrmr-cyber <ritheshrmr@gmail.com>
2026-04-15 23:25:18 +0530

    prediction pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,7 +43,47 @@ class modeltrainer:
                 "catboosting classifier":CatBoostRegressor(verbose=False),
                 "adaboost classifier":AdaBoostRegressor(),
             }
-            model_report :dict=evaluate_models(x_train,y_train,x_test,y_test,models)
+            params={
+                "decision tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "random forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "gradient boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "linear regressor":{},
+                "xgboost classifier":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "catboosting classifier":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "adaboost classifier":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "k-neighbours classifier":{
+                    "n_neighbors":[5,7,9,11]
+                }
+                
+            }
+            model_report :dict=evaluate_models(x_train,y_train,x_test,y_test,models,param = params)
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
